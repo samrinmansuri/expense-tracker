3000bcbfad9d2db617fa2f948cb19f6520242ce1
samrinmansuri <samrinmansuri6623@gmail.com>
2026-08-01 23:11:18 +0530

    add expense category selection menu

--- main.py
+++ main.py
@@ -21,4 +21,22 @@ def main():
     print(f"🎯 Getting User Expense")
     expense_name = input("Enter expense name: ")
     expense_amount = float(input("Enter expense amount: "))
-    
+    expense_categories = [
+        "🍔 Food",
+        "🏠 Home",
+        "💼 Work",
+        "🎉 Fun",
+        "✨ Misc",
+    ]
+
+    while True:
+        print("Select a category: ")
+        for i, category_name in enumerate(expense_categories):
+            print(f"  {i + 1}. {category_name}")
+
+        value_range = f"[1 - {len(expense_categories)}]"
+        selected_index = int(input(f"Enter a category number {value_range}: ")) - 1
+
+        if selected_index in range(len(expense_categories)):
+            selected_category = expense_categories[selected_index]
+            
