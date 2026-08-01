871490ab0983c179f5c80cdf8d1fc1a748b24519
samrinmansuri <samrinmansuri6623@gmail.com>
2026-08-01 22:59:11 +0530

    set up expense tracker main workflow

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from expense import Expense
 import calendar
 import datetime
+def main():
+    print(f"🎯 Running Expense Tracker!")
+    expense_file_path = "expenses.csv"
+    budget = 2000
+
+    # Get user input for expense.
+    expense = get_user_expense()
+
+    # Write their expense to a file.
+    save_expense_to_file(expense, expense_file_path)
+
+    # Read file and summarize expenses.
+    summarize_expenses(expense_file_path, budget)
+    
